ef1e1e1894c24647069e5e79be73d1a24175fc55
Lou Foster <lf6f@virginia.edu>
2019-05-29 15:43:32 -0400

    attempt2

--- main.go
+++ main.go
@@ -4,8 +4,7 @@ import (
 	"fmt"
 	"log"
 
-	"./parser"
-
+   "github.com/uvalib/virgo4-parser"
 	"github.com/antlr/antlr4/runtime/Go/antlr"
 )
 
@@ -13,7 +12,7 @@ func main() {
 	test := `( title : {"susan sontag" OR music title}   AND keyword:{ Maunsell } ) OR author:{ liberty }`
 	log.Printf("Test virgo4 parser with sample input stream %s", test)
 	is := antlr.NewInputStream(test)
-	lexer := parser.NewVirgoQueryLexer(is)
+	lexer := v4parser.NewVirgoQueryLexer(is)
 	for {
 		t := lexer.NextToken()
 		if t.GetTokenType() == antlr.TokenEOF {
