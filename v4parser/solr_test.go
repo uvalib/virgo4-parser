5bdf88506d8cbc3195f308562a943b87f68e9756
Jason Jordan <jlj5aj@virginia.edu>
2020-01-27 14:56:42 -0500

    generalize testing for easier maintenance

--- v4parser/solr_test.go
+++ v4parser/solr_test.go
@@ -6,8 +6,10 @@ import (
 	"github.com/uvalib/virgo4-parser/v4parser"
 )
 
-func convert(query string) (v4parser.SolrParser, string, error) {
+func convert(t *testing.T, query string) (v4parser.SolrParser, string, error) {
 	sp := v4parser.SolrParser{Debug: true}
+
+	t.Logf("QUERY: %s", query)
 
 	solr, err := v4parser.ConvertToSolrWithParser(&sp, query)
 
@@ -15,24 +17,29 @@ func convert(query string) (v4parser.SolrParser, string, error) {
 }
 
 func expectSolrConversionSuccess(t *testing.T, query string, expected string) v4parser.SolrParser {
-	sp, solr, err := convert(query)
+	sp, solr, err := convert(t, query)
 
 	if err != nil {
-		t.Errorf("%s couldn't convert, but should have: %s", query, err.Error())
+		t.Errorf("Conversion failed, but should have succeeded:")
+		t.Errorf("ERROR: %s", err.Error())
 	}
 
 	if solr != expected {
-		t.Errorf("%s convert fail. Expected %s, Actual: %s", query, expected, solr)
+		t.Errorf("Conversion succeeded, but result was not as expected:")
+		t.Errorf("GOT  : %s", solr)
+		t.Errorf("WANT : %s", expected)
 	}
 
 	return sp
 }
 
 func expectSolrConversionFailure(t *testing.T, query string) {
-	_, solr, err := convert(query)
+	_, solr, err := convert(t, query)
 
 	if err == nil {
-		t.Errorf("%s converted to %s, but should have failed", query, solr)
+		t.Errorf("Conversion succeeded, but should have failed:")
+		t.Errorf("QUERY: %s", query)
+		t.Errorf("GOT  : %s", solr)
 	}
 }
 
@@ -44,138 +51,190 @@ func expectCounts(t *testing.T, label string, values []string, want int) {
 	}
 }
 
-func TestSolrSimpleValid(t *testing.T) {
-	q := "title: {bannanas}"
-	e := `_query_:"{!edismax qf=$title_qf pf=$title_pf}(bannanas)"`
+func TestSolrShouldSucceed(t *testing.T) {
+	type expectedResults struct {
+		query string // v4 query
+		solr string // expected solr conversion
+		counts bool // check counts?
+		titles int // expected title count
+		authors int // expected author count
+		subjects int // expected subject count
+		keywords int // expected keyword count
+	}
 
-	expectSolrConversionSuccess(t, q, e)
+	tests := []expectedResults {
+		{
+			query: `keyword:{}`,
+			solr: `_query_:"{!edismax}(*)"`,
+		},
+		{
+			query: `title: {bannanas}`,
+			solr: `_query_:"{!edismax qf=$title_qf pf=$title_pf}(bannanas)"`,
+		},
+		{
+			query: `title:{A = B}`,
+			solr: `_query_:"{!edismax qf=$title_qf pf=$title_pf}(A = B)"`,
+		},
+		{
+			query: `keyword:{digby OR duncan}`,
+			solr: `(_query_:"{!edismax}(digby)" OR _query_:"{!edismax}(duncan)")`,
+		},
+		{
+			query: `keyword:{digby AND duncan}`,
+			solr: `(_query_:"{!edismax}(digby)" AND _query_:"{!edismax}(duncan)")`,
+		},
+		{
+			query: `author:{Zhongguo Zang xue chu ban she  (publisher)}`,
+			solr: `_query_:"{!edismax qf=$author_qf pf=$author_pf}(Zhongguo Zang xue chu ban she publisher)"`,
+		},
+		{
+			query: `author:{ ʼJam-dbyangs-nyi-ma }`,
+			solr: `_query_:"{!edismax qf=$author_qf pf=$author_pf}(ʼJam-dbyangs-nyi-ma)"`,
+		},
+		{
+			query: `title:{Tragicheskai͡a istorii͡a kavkazskikh }`,
+			solr: `_query_:"{!edismax qf=$title_qf pf=$title_pf}(Tragicheskai͡a istorii͡a kavkazskikh)"`,
+		},
+		{
+			query: `author:{Немирович-Данченко, Василий Иванович}`,
+			solr: `_query_:"{!edismax qf=$author_qf pf=$author_pf}(Немирович-Данченко, Василий Иванович)"`,
+		},
+		{
+			query: `title : {"susan sontag" OR (music title)}   AND keyword:{ Maunsell }`,
+			solr: `((_query_:"{!edismax qf=$title_qf pf=$title_pf}(\"susan sontag\")" OR _query_:"{!edismax qf=$title_qf pf=$title_pf}(music title)") AND _query_:"{!edismax}(Maunsell)")`,
+		},
+		{
+			query: `( title : {"susan sontag" OR music title}   AND keyword:{ Maunsell } ) OR author:{ liberty }`,
+			solr: `((((_query_:"{!edismax qf=$title_qf pf=$title_pf}(\"susan sontag\")" OR _query_:"{!edismax qf=$title_qf pf=$title_pf}(music title)") AND _query_:"{!edismax}(Maunsell)")) OR _query_:"{!edismax qf=$author_qf pf=$author_pf}(liberty)")`,
+		},
+		{
+			query: `( title : {"susan sontag" OR (music title)}   AND keyword:{ Maunsell } ) OR author:{ liberty }`,
+			solr: `((((_query_:"{!edismax qf=$title_qf pf=$title_pf}(\"susan sontag\")" OR _query_:"{!edismax qf=$title_qf pf=$title_pf}(music title)") AND _query_:"{!edismax}(Maunsell)")) OR _query_:"{!edismax qf=$author_qf pf=$author_pf}(liberty)")`,
+		},
+		{
+			query: `title : {"susan sontag" OR (music title)}   AND ( keyword:{ Maunsell } OR author:{ liberty })`,
+			solr: `((_query_:"{!edismax qf=$title_qf pf=$title_pf}(\"susan sontag\")" OR _query_:"{!edismax qf=$title_qf pf=$title_pf}(music title)") AND ((_query_:"{!edismax}(Maunsell)" OR _query_:"{!edismax qf=$author_qf pf=$author_pf}(liberty)")))`,
+		},
+		{
+			query: ` author: {lovecraft} AND title: {madness NOT dunwich}`,
+			solr: `(_query_:"{!edismax qf=$author_qf pf=$author_pf}(lovecraft)" AND (_query_:"{!edismax qf=$title_qf pf=$title_pf}(madness)" NOT _query_:"{!edismax qf=$title_qf pf=$title_pf}(dunwich)"))`,
+		},
+		{
+			query: `identifier:{35007007606860  OR 9780754645733 OR 38083649 OR 2001020407  OR u5670758 OR "KJE5602.C73 2012"}`,
+			solr: `(((((_query_:"{!edismax qf=$identifier_qf pf=$identifier_pf}(35007007606860)" OR _query_:"{!edismax qf=$identifier_qf pf=$identifier_pf}(9780754645733)") OR _query_:"{!edismax qf=$identifier_qf pf=$identifier_pf}(38083649)") OR _query_:"{!edismax qf=$identifier_qf pf=$identifier_pf}(2001020407)") OR _query_:"{!edismax qf=$identifier_qf pf=$identifier_pf}(u5670758)") OR _query_:"{!edismax qf=$identifier_qf pf=$identifier_pf}(\"KJE5602.C73 2012\")")`,
+		},
+		{
+			query: `keyword:{triceratops OR flameproof}`,
+			solr: `(_query_:"{!edismax}(triceratops)" OR _query_:"{!edismax}(flameproof)")`,
+		},
+		{
+			query: `identifier:{35007007606860}`,
+			solr: `_query_:"{!edismax qf=$identifier_qf pf=$identifier_pf}(35007007606860)"`,
+		},
+		{
+			query: `( title: {pepperoni OR artichoke hearts} AND subject:{pizza} ) OR (subject:{calzone} AND (keyword:{italian} NOT author:{fieri}))`,
+			solr: `((((_query_:"{!edismax qf=$title_qf pf=$title_pf}(pepperoni)" OR _query_:"{!edismax qf=$title_qf pf=$title_pf}(artichoke hearts)") AND _query_:"{!edismax qf=$subject_qf pf=$subject_pf}(pizza)")) OR ((_query_:"{!edismax qf=$subject_qf pf=$subject_pf}(calzone)" AND ((_query_:"{!edismax}(italian)" NOT _query_:"{!edismax qf=$author_qf pf=$author_pf}(fieri)")))))`,
+			counts: true,
+			titles: 2,
+			authors: 1,
+			subjects: 2,
+			keywords: 1,
+		},
+		{
+			query: `date:{1945}`,
+			solr: `_query_:"{!lucene df=published_daterange}(1945)"`,
+		},
+		{
+			query: `date:{1945/12/07 TO 1949}`,
+			solr: `_query_:"{!lucene df=published_daterange}([1945-12-07 TO 1949])"`,
+		},
+		{
+			query: `date:{BEFORE 1945-12-06}`,
+			solr: `_query_:"{!lucene df=published_daterange}([* TO 1945-12-06])"`,
+		},
+		{
+			query: `date:{AFTER 1945}`,
+			solr: `_query_:"{!lucene df=published_daterange}([1945 TO *])"`,
+		},
+		{
+			query: `date:{<1945} AND date:{>1932} AND author:{Shelly}`,
+			solr: `((_query_:"{!lucene df=published_daterange}([* TO 1945])" AND _query_:"{!lucene df=published_daterange}([1932 TO *])") AND _query_:"{!edismax qf=$author_qf pf=$author_pf}(Shelly)")`,
+		},
+		{
+			query: `keyword:{cincinnati, ohio (home of the :reds:)}`,
+			solr: `_query_:"{!edismax}(cincinnati, ohio home of the :reds:)"`,
+		},
+		{
+			query: `keyword: {"grapes of wrath"}`,
+			solr: `_query_:"{!edismax}(\"grapes of wrath\")"`,
+		},
+		{
+			query: `keyword: {kyoto NOT protocol}`,
+			solr: `(_query_:"{!edismax}(kyoto)" NOT _query_:"{!edismax}(protocol)")`,
+		},
+		{
+			query: `keyword: {"frida kahlo" AND exhibitions}`,
+			solr: `(_query_:"{!edismax}(\"frida kahlo\")" AND _query_:"{!edismax}(exhibitions)")`,
+		},
+		{
+			query: `keyword: {(calico OR "tortoise shell") AND cats}`,
+			solr: `((_query_:"{!edismax}(calico)" OR _query_:"{!edismax}(\"tortoise shell\")") AND _query_:"{!edismax}(cats)")`,
+		},
+		{
+			query: ` keyword : { (calico OR tortoise shell) AND cats } `,
+			solr: `((_query_:"{!edismax}(calico)" OR _query_:"{!edismax}(tortoise shell)") AND _query_:"{!edismax}(cats)")`,
+		},
+		{
+			query: `(keyword: {calico OR "tortoise shell"})  AND keyword: {cats}`,
+			solr: `(((_query_:"{!edismax}(calico)" OR _query_:"{!edismax}(\"tortoise shell\")")) AND _query_:"{!edismax}(cats)")`,
+		},
+		{
+			query: ` keyword : { ("tortoise shell" OR calico) AND cats } `,
+			solr: `((_query_:"{!edismax}(\"tortoise shell\")" OR _query_:"{!edismax}(calico)") AND _query_:"{!edismax}(cats)")`,
+		},
+		{
+			query: `keyword: {(calico OR "tortoise shell") AND (cats OR dogs)}`,
+			solr: `((_query_:"{!edismax}(calico)" OR _query_:"{!edismax}(\"tortoise shell\")") AND (_query_:"{!edismax}(cats)" OR _query_:"{!edismax}(dogs)"))`,
+		},
+		{
+			query: `keyword:{ \" rotunda \" }`,
+			solr: `_query_:"{!edismax}(\ \"rotunda \\")"`,
+		},
+	}
+
+	for _, test := range tests {
+		sp := expectSolrConversionSuccess(t, test.query, test.solr)
+
+		if test.counts == true {
+			expectCounts(t, "title", sp.Titles, test.titles)
+			expectCounts(t, "author", sp.Authors, test.authors)
+			expectCounts(t, "subject", sp.Subjects, test.subjects)
+			expectCounts(t, "keyword", sp.Keywords, test.keywords)
+		}
+	}
 }
 
-func TestSolrSpecialCharsValid(t *testing.T) {
-	q := "title:{A = B}"
-	e := `_query_:"{!edismax qf=$title_qf pf=$title_pf}(A = B)"`
+func TestSolrShouldFail(t *testing.T) {
+	tests := []string {
+		``,
+		`(`,
+		`)`,
+		`()`,
+		`author:`,
+		`subject:{`,
+		`identifier:}`,
+		`keyword:{tacos`,
+		`(keyword:{derivatives}`,
+		`title: {bannanas} OR author: bad`,
+		`title:{bananas ( a fruit }`,
+		`title:{bananas a fruit ) }`,
+		`garbage:{1954}`,
+		`rubbish:{bananas}`,
+		`date:{1932 TO 1945} HELLOOOOO author:{Shelly}`,
+		`date:{BadDate}`,
+	}
 
-	expectSolrConversionSuccess(t, q, e)
-}
-
-func TestSolrIdentifierValid(t *testing.T) {
-	q := `identifier:{35007007606860}`
-	e := `_query_:"{!edismax qf=$identifier_qf pf=$identifier_pf}(35007007606860)"`
-
-	expectSolrConversionSuccess(t, q, e)
-}
-
-func TestSolrSimpleInvalid(t *testing.T) {
-	q := "title: {bannanas} OR author: bad"
-
-	expectSolrConversionFailure(t, q)
-}
-
-func TestSolrValid(t *testing.T) {
-	q := `( title : {"susan sontag" OR music title}   AND keyword:{ Maunsell } ) OR author:{ liberty }`
-	e := `((((_query_:"{!edismax qf=$title_qf pf=$title_pf}(\"susan sontag\")" OR _query_:"{!edismax qf=$title_qf pf=$title_pf}(music title)") AND _query_:"{!edismax}(Maunsell)")) OR _query_:"{!edismax qf=$author_qf pf=$author_pf}(liberty)")`
-
-	expectSolrConversionSuccess(t, q, e)
-}
-
-func TestSolrValidCounts(t *testing.T) {
-	q := `( title: {pepperoni OR artichoke hearts} AND subject:{pizza} ) OR (subject:{calzone} AND (keyword:{italian} NOT author:{fieri}))`
-	e := `((((_query_:"{!edismax qf=$title_qf pf=$title_pf}(pepperoni)" OR _query_:"{!edismax qf=$title_qf pf=$title_pf}(artichoke hearts)") AND _query_:"{!edismax qf=$subject_qf pf=$subject_pf}(pizza)")) OR ((_query_:"{!edismax qf=$subject_qf pf=$subject_pf}(calzone)" AND ((_query_:"{!edismax}(italian)" NOT _query_:"{!edismax qf=$author_qf pf=$author_pf}(fieri)")))))`
-
-	sp := expectSolrConversionSuccess(t, q, e)
-
-	expectCounts(t, "title", sp.Titles, 2)
-	expectCounts(t, "author", sp.Authors, 1)
-	expectCounts(t, "subject", sp.Subjects, 2)
-	expectCounts(t, "keyword", sp.Keywords, 1)
-}
-
-func TestSolrDateSingle(t *testing.T) {
-	q := `date:{1945}`
-	e := `_query_:"{!lucene df=published_daterange}(1945)"`
-
-	expectSolrConversionSuccess(t, q, e)
-}
-
-func TestSolrDateRange(t *testing.T) {
-	q := `date:{1945/12/07 TO 1949}`
-	e := `_query_:"{!lucene df=published_daterange}([1945-12-07 TO 1949])"`
-
-	expectSolrConversionSuccess(t, q, e)
-}
-
-func TestSolrDateBefore(t *testing.T) {
-	q := `date:{BEFORE 1945-12-06}`
-	e := `_query_:"{!lucene df=published_daterange}([* TO 1945-12-06])"`
-
-	expectSolrConversionSuccess(t, q, e)
-}
-
-func TestSolrDateAfter(t *testing.T) {
-	q := `date:{AFTER 1945}`
-	e := `_query_:"{!lucene df=published_daterange}([1945 TO *])"`
-
-	expectSolrConversionSuccess(t, q, e)
-}
-
-func TestSolrDateMixed(t *testing.T) {
-	q := `date:{<1945} AND date:{>1932} AND author:{Shelly}`
-	e := `((_query_:"{!lucene df=published_daterange}([* TO 1945])" AND _query_:"{!lucene df=published_daterange}([1932 TO *])") AND _query_:"{!edismax qf=$author_qf pf=$author_pf}(Shelly)")`
-
-	expectSolrConversionSuccess(t, q, e)
-}
-
-func TestSolrEmptyQuery(t *testing.T) {
-	q := ``
-
-	expectSolrConversionFailure(t, q)
-}
-
-func TestSolrEmptyKeyword(t *testing.T) {
-	q := `keyword:{}`
-	e := `_query_:"{!edismax}(*)"`
-
-	expectSolrConversionSuccess(t, q, e)
-}
-
-func TestSolrBobRandomQuery(t *testing.T) {
-	q := `keyword:{cincinnati, ohio (home of the :reds:)}`
-	e := `_query_:"{!edismax}(cincinnati, ohio home of the :reds:)"`
-
-	expectSolrConversionSuccess(t, q, e)
-}
-
-func TestSolrSearchTip1(t *testing.T) {
-	q := `keyword: {"grapes of wrath"}`
-	e := `_query_:"{!edismax}(\"grapes of wrath\")"`
-
-	expectSolrConversionSuccess(t, q, e)
-}
-
-func TestSolrSearchTip2(t *testing.T) {
-	q := `keyword: {kyoto NOT protocol}`
-	e := `(_query_:"{!edismax}(kyoto)" NOT _query_:"{!edismax}(protocol)")`
-
-	expectSolrConversionSuccess(t, q, e)
-}
-
-func TestSolrSearchTip3(t *testing.T) {
-	q := `keyword: {"frida kahlo" AND exhibitions}`
-	e := `(_query_:"{!edismax}(\"frida kahlo\")" AND _query_:"{!edismax}(exhibitions)")`
-
-	expectSolrConversionSuccess(t, q, e)
-}
-
-func TestSolrSearchTip4a(t *testing.T) {
-	q := `keyword: {(calico OR "tortoise shell") AND cats}`
-	e := `((_query_:"{!edismax}(calico)" OR _query_:"{!edismax}(\"tortoise shell\")") AND _query_:"{!edismax}(cats)")`
-
-	expectSolrConversionSuccess(t, q, e)
-}
-
-func TestSolrSearchTip4b(t *testing.T) {
-	q := `(keyword: {calico OR "tortoise shell"})  AND keyword: {cats}`
-	e := `(((_query_:"{!edismax}(calico)" OR _query_:"{!edismax}(\"tortoise shell\")")) AND _query_:"{!edismax}(cats)")`
-
-	expectSolrConversionSuccess(t, q, e)
+	for _, query := range tests {
+		expectSolrConversionFailure(t, query)
+	}
 }
